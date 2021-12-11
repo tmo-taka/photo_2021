1242ce294e837a9ec3581cc1d3a6e8a0c437f206
takahara tomoya <t.tkorock221@gmail.com>
2021-12-12 08:46:33 +0900

    これでどうだ

--- component/module/header.tsx
+++ component/module/header.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import HeaderList from '@component/atoms/list'
+// import styles from '@style/module/header.module.scss'
 import styles from '@style/module/header.module.scss'
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.headerIn}>
-        <div>新しいポートフォリオテスト</div>
-        <HeaderList/>
-      </div>
-    </header>
+      <header className={styles.header}>
+        <div className={styles.headerIn}>
+          <div>新しいポートフォリオテスト</div>
+          <HeaderList/>
+        </div>
+      </header>
     )
 }
 
