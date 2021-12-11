d43164abe904adce5d6611dd967de63662c88df1
takahara tomoya <t.tkorock221@gmail.com>
2021-12-11 23:52:00 +0900

    header再度

--- component/module/header.tsx
+++ component/module/header.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import HeaderList from '@component/atoms/list'
-// import styles from '@style/module/header.module.scss'
+import styles from '@style/module/header.module.scss'
 
 const Header = () => {
   return (
-    // <header className={styles.header}>
-    //   <div className={styles.headerIn}>
-    <div>
+    <header className={styles.header}>
+      <div className={styles.headerIn}>
         <div>新しいポートフォリオテスト</div>
         <HeaderList/>
-    </div>
-    //   </div>
-    // </header>
+      </div>
+    </header>
     )
 }
 
