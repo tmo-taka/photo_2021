fba5c64c450cd1d3e77ba36440883d9275b97d6c
takahara tomoya <t.tkorock221@gmail.com>
2021-12-12 09:17:34 +0900

    勝手に大文字になる

--- component/module/header.tsx
+++ component/module/header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeaderList from '@component/atoms/list'
-import styles from '@style/module/header.module.scss'
+import styles from '@style/module/header_.module.scss'
 
 const Header = () => {
   return (
