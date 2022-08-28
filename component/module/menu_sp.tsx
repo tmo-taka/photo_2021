5868d69a2d30651a682de2650ac9fbe799c4cf51
takahara tomoya <t.tkorock221@gmail.com>
2022-08-28 21:24:34 +0900

    menuにemotionを組み込んで一部分のみ対応

--- component/module/menu_sp.tsx
+++ component/module/menu_sp.tsx
@@ -1,28 +1,41 @@
-import React from 'react'
+import { ReactNode } from 'react'
 import { useState } from 'react'
 import MenuList from '@component/atoms/list'
 import styles from '@style/module/menu_sp.module.scss'
+import { css } from '@emotion/react'
 
 type Props = {
+    children?: ReactNode,
     displayFlag ?: Boolean
 }
 
-const Menu = ({displayFlag}: Props) => {
+type Border = {
+    position: "top" | "cen" | "bot",
+    active: SerializedStyles
+}
+
+const Menu = (props:Props) => {
 
     const [menuFlag,setMenuFlag] = useState<boolean>(false)
 
-    const menuControl = function() {
-        setMenuFlag(!menuFlag);
-    }
+    const border:Border[] = [
+        {position: "top", active: style.border.active.top},
+        {position: "cen", active: style.border.active.cen},
+        {position: "bot", active: style.border.active.bot},
+    ]
+
+    const menuControl = () => setMenuFlag(!menuFlag);
 
     return (
         <div>
-            <div className={`${styles.menu} ${displayFlag ? "" : styles.on}`} onClick={menuControl}>
+            <div className={`${styles.menu} ${props.displayFlag ? "" : styles.on}`} onClick={() =>menuControl()}>
                 <div className={`${styles.menu__icon} ${menuFlag ? styles.on : ""}`}>
                     <div className={styles.menu__icon__border}>
-                        <span className={styles.menu__icon__border__top}></span>
-                        <span className={styles.menu__icon__border__cen}></span>
-                        <span className={styles.menu__icon__border__bot}></span>
+                        {
+                            border.map((data:Border) => {
+                                return <span key={data.position} css={[style.border.base, menuFlag ? data.active :  ""]}></span>
+                            })
+                        }
                     </div>
                 </div>
                 <nav className={`${styles.menu__nav} ${menuFlag ? styles.on : ""}`}>
@@ -36,3 +49,45 @@ const Menu = ({displayFlag}: Props) => {
 }
 
 export default Menu;
+
+
+const style = {
+    border: {
+        base: css`
+            margin: 0 auto .22em;
+            display: block;
+            width: 1.8em;
+            height: .2em;
+            background-color: #FFF;
+            transition: .2s ease-in;
+        `,
+        top: css`
+            transform-origin: top left;
+            transform: translate(.4em,0) rotate(45deg);
+            width: 2.2em;
+        `,
+        cen: css`
+            opacity: 0;
+        `,
+        bot: css`
+            transform-origin: bottom left;
+            transform: translate(.4em,.5em) rotate(-45deg);
+            width: 2.2em;
+        `,
+        active: {
+            top: css`
+                transform-origin: top left;
+                transform: translate(.4em,0) rotate(45deg);
+                width: 2.2em;
+            `,
+            cen: css`
+                opacity: 0;
+            `,
+            bot: css`
+                transform-origin: bottom left;
+                transform: translate(.4em,.5em) rotate(-45deg);
+                width: 2.2em;
+            `
+        }
+    }
+}
